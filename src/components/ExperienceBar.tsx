de718ce390d266ee1ec447b3f7832ed9a5c21642
Robson Fischer <fischerrobson@gmail.com>
2021-03-08 15:17:30 -0300

    deploy da aplicacao

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,21 +1,30 @@
-import styles from '../styles/components/ExperienceBar.module.css'
-import { useContext} from 'react'
-import { ChallengesContext } from '../contexts/ChallengesContext'
+import styles from "../styles/components/ExperienceBar.module.css";
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
 
-export default function ExperienceBar () {
+export default function ExperienceBar() {
+  const { currentExperience, experienceToNextLevel } = useContext(
+    ChallengesContext
+  );
 
-    const { currentExperience, experienceToNextLevel } = useContext(ChallengesContext)
+  const percentToNextLevel =
+    Math.round(currentExperience * 100) / experienceToNextLevel;
 
-    const percentToNextLevel = Math.round(currentExperience * 100) / experienceToNextLevel
-
-    return (
-        <header className={styles.experienceBar}>
-            <span>0 xp</span>
-            <div>
-                <div style={{width: `${percentToNextLevel}%`}} />
-                <span className={styles.currentExperience} style={{left: `${percentToNextLevel}%` }}>{currentExperience} xp</span>
-            </div>
-            <span>{experienceToNextLevel} xp</span>
-        </header>
-    )
+  return (
+    <header className={styles.experienceBar}>
+      <span>0 xp</span>
+      <div>
+        <div
+          style={{ width: `${percentToNextLevel}%`, transition: "width 0.2s" }}
+        />
+        <span
+          className={styles.currentExperience}
+          style={{ left: `${percentToNextLevel}%` }}
+        >
+          {currentExperience} xp
+        </span>
+      </div>
+      <span>{experienceToNextLevel} xp</span>
+    </header>
+  );
 }
